f10780208eccd3f81061c576b82cc8ad8195a07b
Vosty17 <stephwafula70@gmail.com>
2025-04-25 17:18:25 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -8,3 +8,25 @@ FROM
 JOIN 
     (SELECT 1 AS n UNION ALL SELECT 2 UNION ALL SELECT 3) numbers
     ON CHAR_LENGTH(Products) - CHAR_LENGTH(REPLACE(Products, ',', '')) >= numbers.n - 1;
+-- Question 2
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE OrderDetails (
+    OrderID INT,
+    Product VARCHAR(50),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT DISTINCT OrderID, CustomerName
+FROM OriginalTable;
+
+INSERT INTO OrderDetails (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OriginalTable;
